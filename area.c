ca70e0c1d7147bd35c0b95d6bd18abaabdf8b518
Dhruvsn <121716381+Dhruvsn@users.noreply.github.com>
2022-12-31 16:24:43 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+   int a,b;
+   printf("enter the lenth");
+   scanf("%d",&a);
+   printf("enter the breath");
+   scanf("%d",&b);
+   printf("area of rectangle %d",a*b);
+   return 0;
+}

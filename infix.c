ba1e08b6582a0da24484b3135f07eeaf61da691f
Dhruvsn <121716381+Dhruvsn@users.noreply.github.com>
2023-04-27 11:29:49 +0530

    Add files via upload

--- infix.c
+++ infix.c
@@ -1,57 +1,160 @@
-// program to evaluate postfix expression using stacks.
-#include<stdio.h>
-#include<conio.h>
-struct stack
-{
-  int top;
-  float a[50];
-  }s;
-  int main(){
-   char pf[50];
-   float d1,d2,d3;
-   int i;
-   //drscr();
-  s.top = -1;
-    printf("\n\n Enter the postfix expression:"0;
-     gets(pf);
-     for(i=0;pf[i]!='\0';i++){
-       switch(pf[i]){
-         case '0':
-         case '1':
-         case '2':
-         case '3':
-         case'4':
-         case '5':
-         case '6':
-         case '7':
-         case '8':
-         case '9':
-        s.a[++s.top] = pf[i]-'0';
-        break;
-       case '+':
-          d1 = s.a[s.top--];
-          d2 = s.a[s.top--];
-         s.a[++s.top] = d1 + d2;
-        break;
+// program to evaluate infix exprression using stack.
 
-        case '-': 
-         d2 = s.a[s.top--];
-         d1 = s.a[s.top--];
-          s.a[++s.top] = d1 - d2;
-         break;
-         case '*':  
-       d2 = s.a[s.top--];
-       d1 = s.a[s.top--];
-        s.a[++s.top] = d1*d2;
-       break;
-        case '/':
-       d2 = s.a[s.top--];
-      d1 = s.a[s.top--];
-      s.a[++s.top] = d1 / d2;
-       break;
- }
+
+// header files
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+//Global variables
+int numbers[50],tn=-1,to=-1;
+char op[50];
+//function to push digits
+void push_num(int n)
+{
+numbers[++tn]=n;
 }
-printf("\n Expression value is %5.2f", s.a[s.top]);
-getch();
+//function to push operators
+void push_op(char ch)
+{
+op[++to]=ch;
+}
+// function to pop digits
+int pop_num()
+{
+return numbers[tn--];
+}
+//function to pop operators
+char pop_op()
+{
+return op[to--];
+}
+//evaluating the expression
+int infix_eval(int numbers[50], char op[50])
+{
+int x, y;
+char ope;
+//taking first two operands
+x=pop_num();
+y=pop_num();
+//taking the operators between them
+ope=pop_op();
+//executing the operation
+switch(ope)
+{
+case '+':
+    return x+y;
+case '-':
+    return y-x;
+case '*':
+    return x*y;
+case '/':
+    if(x==0)
+    {
+        printf("\n cannot divide by 0");
+        exit(0);
+    }
+    else
+        return y/x;
+}
 return 0;
+}
+//Function to check whether the character is an operator or not
+int is_operator(char ch)
+{
+    return (ch=='+'||ch=='-'||ch=='*'||ch=='/'||ch=='^');
+}
+//the precedence of the operators
+int precedence (char c)
+{
+    switch (c)
+    {
+    case '+':
+        return 1;
+    case '-':
+        return 1;
+    case '*':
+        return 2;
+    case '/':
+        return 2;
+    case '^':
+        return 3;
+    }
+    return -1;
+}
+//function to evaluate an infix expression
+int eval(char exp[20])
+{
+    int i, num, output,r;
+    char c;
+    for (i=0;exp[i]!='\0';i++)
+    {
+        //taking each character from the expression
+        c = exp[i];
+       //checking if it is number
+       if(isdigit(c)!=0)
+       {
+           num = 0;
+           while (isdigit(c))
+           {
+               num = num*10 + (c-'0');
+               i++;
+               if (i<strlen(exp))
+                c = exp[i];
+               else
+                break;
+           }
+           i--;
+           //pushing the number into stack of numbers
+           push_num(num);
+       }
+       else if(c=='(')
+       {
+           //pushing the operators into the stack
+           push_op(c);
+       }
+       //If we get close bracket, evaluate the entire brackets
+       else if(c==')')
+       {
+           while (op[to]!='(')
+           {
+               r = infix_eval (numbers, op);
+               //pushing the result back to stack
+               push_num(r);
+           }
+           pop_op();
+       }
+       //if the current character is operator
+       else if (is_operator(c))
+       {
+           //evaluating the expression
+           while (to!=-1 && precedence (c)<= precedence(op[to]))
+           {
+               output = infix_eval(numbers,op);
+               //pushing the result back to stack
+               push_num(output);
+           }
+           //pushing the current operator to stack
+           push_op(c);
+       }
+    }
+    //if there is any remaining expression, evaluate them
+    while(to!=-1)
+    {
+        output = infix_eval(numbers,op);
+        //pushing it back to stack
+        push_num(output);
+    }
+    return pop_num();
+}
+//Main function
+int main()
+{
+    char exp[50];
+    //taking the expression to evaluate
+    printf("Enter the infix expression to evaluate:");
+    gets(exp);
+    //calling the function and printing the result
+    printf("%d",eval(exp));
+    return 0;
 }
